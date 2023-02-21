2dfbb28cfea25c5de1107bbb57012ae2945065ea
TeachFakerPlayingMid <1310423707@qq.com>
2023-02-21 18:40:16 -0500

    createbl.sql finished.Checking is required based on ER dropbl finished. The dropping sequence required checking add some data, till match add all data, need check

--- createtbl.sql
+++ createtbl.sql
@@ -2,7 +2,7 @@
 -- Make sure to terminate each statement with a semicolon (;)
 
 -- LEAVE this statement on. It is required to connect to your database.
-CONNECT TO cs421;
+--CONNECT TO cs421;
 
 -- Remember to put the create table ddls for the tables with foreign key references
 --    ONLY AFTER the parent tables has already been created.
@@ -16,7 +16,7 @@ CREATE TABLE Team
     country VARCHAR(30) NOT NULL ,
     PRIMARY KEY (country),
     national_association VARCHAR(50) NOT NULL,
-    url VARCHAR(50) NOT NULL ,
+    url VARCHAR(100) NOT NULL ,
     group VARCHAR(1) NOT NULL
 );
 
@@ -24,6 +24,7 @@ CREATE TABLE Player
 (
     pid INTEGER NOT NULL ,
     PRIMARY KEY (pid),
+    name VARCHAR(30) NOT NULL ,
     DOB DATE NOT NULL,
     shirt_number INTEGER NOT NULL ,
     general_position VARCHAR(10) NOT NULL,
@@ -41,6 +42,16 @@ CREATE TABLE Coach
     FOREIGN KEY(country) REFERENCES Team(country)
 );
 
+CREATE TABLE Stadium
+(
+    name VARCHAR(50) NOT NULL ,
+    PRIMARY KEY (name),
+    location VARCHAR(50) NOT NULL,
+    entrance_info VARCHAR(50),
+    capacity INTEGER NOT NULL ,
+    parking_info VARCHAR(50)
+);
+
 CREATE TABLE Match
 (
     identidier INTEGER NOT NULL ,
@@ -49,11 +60,26 @@ CREATE TABLE Match
     time TIME NOT NULL ,
     round VARCHAR(10),
     extra_program VARCHAR(30),
-    "availiability" INTEGER,
+    availability INTEGER,
     match_length INTEGER NOT NULL ,
     match_name VARCHAR(50),
     stadium VARCHAR(50),
     FOREIGN KEY (stadium) REFERENCES Stadium(name)
+);
+
+CREATE TABLE Match_Player
+(
+    pid INTEGER NOT NULL ,
+    identidier INTEGER NOT NULL ,
+    PRIMARY KEY (pid,identidier),
+    attendance BOOLEAN NOT NULL ,
+    entered_time VARCHAR(10)  ,
+    left_time VARCHAR(10)  ,
+    detailed_position_in_game VARCHAR(10) ,
+    yellow_card_number INTEGER NOT NULL ,
+    red_card_number INTEGER NOT NULL ,
+    FOREIGN KEY (pid) REFERENCES Player(pid),
+    FOREIGN KEY (identidier) REFERENCES Match(identidier)
 );
 
 CREATE TABLE Referees
@@ -62,7 +88,7 @@ CREATE TABLE Referees
     PRIMARY KEY (rid),
     name VARCHAR(30) NOT NULL ,
     year_of_experience INTEGER NOT NULL ,
-    country VARCHAR(30) NOT NULL ,
+    country VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE Referees_assign
@@ -81,18 +107,10 @@ CREATE TABLE MatchGoals
     identidier INTEGER NOT NULL ,
     PRIMARY KEY (identidier,occurrence),
     occurred_time VARCHAR(10) NOT NULL ,
+    who_scored INTEGER NOT NULL ,
+    FOREIGN KEY (who_scored) REFERENCES Player(pid),
     whether_penalty BOOLEAN NOT NULL,
     FOREIGN KEY (identidier) REFERENCES Match(identidier)
-);
-
-CREATE TABLE Stadium
-(
-    name VARCHAR(50) NOT NULL ,
-    PRIMARY KEY (name),
-    location VARCHAR(50) NOT NULL,
-    entrance_info VARCHAR(50),
-    capacity INTEGER NOT NULL ,
-    parking_info VARCHAR(50)
 );
 
 CREATE TABLE Tickets
@@ -105,6 +123,17 @@ CREATE TABLE Tickets
     ticket_type VARCHAR(10) NOT NULL ,
     identidier INTEGER NOT NULL ,
     FOREIGN KEY (identidier) REFERENCES Match (identidier)
+);
+
+CREATE TABLE Payment
+(
+    card_num INTEGER NOT NULL ,
+    PRIMARY KEY (card_num),
+    customer_num INTEGER NOT NULL ,
+    card_type VARCHAR(10) NOT NULL ,
+    security_code INTEGER NOT NULL,
+    expiration_data DATE NOT NULL
+    --FOREIGN KEY (customer_num) REFERENCES Customer(customer_num)
 );
 
 CREATE TABLE Customer
@@ -122,17 +151,10 @@ CREATE TABLE Customer
     DOB DATE NOT NULL ,
     address VARCHAR(50) NOT NULL ,
     Eaddress VARCHAR(30) NOT NULL ,
-    card_num INTEGER NOT NULL ,
-    FOREIGN KEY (card_num) REFERENCES PaymentMethod(card_num)
+    card_num INTEGER ,
+    card_type VARCHAR(10) ,
+    security_code INTEGER,
+    expiration_data DATE
 );
 
-CREATE TABLE Payment
-(
-    card_num INTEGER NOT NULL ,
-    PRIMARY KEY (card_num),
-    customer_num INTEGER NOT NULL ,
-    card_type VARCHAR(10) NOT NULL ,
-    security_code INTEGER NOT NULL,
-    expiration_data DATE NOT NULL ,
-    FOREIGN KEY (customer_num) REFERENCES Customer(customer_num)
-);
+
